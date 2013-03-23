1896c7191f7fa89a830c25625d1d195708d1fc63
AtoCrafter <ato.crafter@gmail.com>
2013-03-23 11:13:03 +0900

    計測結果を表示できるように

--- common/ato/quickmeasure/measure/MeasurePoint.java
+++ common/ato/quickmeasure/measure/MeasurePoint.java
@@ -1,17 +1,41 @@
 package ato.quickmeasure.measure;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.MovingObjectPosition;
+
 /**
  * 座標の計測
  */
 public class MeasurePoint extends Measure {
 
+    /**
+     * 計測開始点
+     */
+    private int ox, oy, oz;
+
     @Override
     public String getText() {
-        return "test";
+        MovingObjectPosition mo = Minecraft.getMinecraft().objectMouseOver;
+        if (mo != null) {
+            int dx = mo.blockX - ox;
+            int dy = mo.blockY - oy;
+            int dz = mo.blockZ - oz;
+            return "(" + dx + "," + dy + "," + dz + ")";
+        } else {
+            return null;
+        }
     }
 
     @Override
     public void start() {
+        MovingObjectPosition mo = Minecraft.getMinecraft().objectMouseOver;
+        if (mo != null) {
+            ox = mo.blockX;
+            oy = mo.blockY;
+            oz = mo.blockZ;
+        } else {
+            oz = oy = oz = 0;
+        }
     }
 
     @Override
