bca5d39219462298857ccd99247874936c9d8dff
AtoCrafter <ato.crafter@gmail.com>
2013-11-03 14:26:43 +0900

    視線の先のブロックの表面にグリッドラインが表示されるように

--- common/ato/quickmeasure/measure/MeasureGrid.java
+++ common/ato/quickmeasure/measure/MeasureGrid.java
@@ -31,7 +31,24 @@ public class MeasureGrid extends Measure {
             num = 10;
         }
         if (world != null && num > 0 && omo.typeOfHit == EnumMovingObjectType.TILE) {
-            entity = new EntityMeasure(world, omo.blockX, omo.blockY, omo.blockZ, num);
+            int x = omo.blockX, y = omo.blockY, z = omo.blockZ;
+            System.out.println(omo.sideHit);
+            switch (omo.sideHit) {
+                case 0: // Bottom
+                case 2: // North
+                case 4: // West
+                    break;
+                case 5: // East
+                    x += 1;
+                    break;
+                case 1: // Top
+                    y += 1;
+                    break;
+                case 3: // South
+                    z += 1;
+                    break;
+            }
+            entity = new EntityMeasure(world, x, y, z, num);
             world.spawnEntityInWorld(entity);
             super.start();
         }
